ebb7d2c732fc01bcb9c91914bbb2b38217fb3aa6
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-07 14:54:00 +0200

    feat: insere função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    file_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas":  len(file),
+        "linhas_do_arquivo": file
+    }
+
+    if len(instance.queue) == 0:
+        instance.enqueue(file_process)
+    else:
+        for process in instance.queue:
+            if path_file != process['nome_do_arquivo']:
+                instance.enqueue(file_process)
+
+    print(file_process, file=sys.stdout)
 
 
 def remove(instance):
