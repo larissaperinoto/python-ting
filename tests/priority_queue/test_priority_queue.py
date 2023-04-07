ccb6d8659c93bf6cdf801566aa8671a561db0e1e
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-07 15:53:23 +0200

    test: insere teste para a classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,37 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+
+process_queue = [
+    {
+        "nome_do_arquivo": "arquivo_teste.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": ["um parágrafo qualquer"]
+    },
+    {
+        "nome_do_arquivo": "arquivo_teste.txt",
+        "qtd_linhas": 10,
+        "linhas_do_arquivo": ["um parágrafo qualquer"]
+    }
+
+]
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    instance = PriorityQueue()
+
+    assert instance.is_priority(process_queue[0]) is True
+    assert instance.is_priority(process_queue[1]) is False
+
+    instance.enqueue(process_queue[0])
+    instance.enqueue(process_queue[1])
+    assert len(instance.high_priority) == 1
+    assert len(instance.regular_priority) == 1
+
+    assert instance.search(0) == process_queue[0]
+
+    instance.dequeue()
+    assert len(instance.high_priority) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        instance.search(10)
