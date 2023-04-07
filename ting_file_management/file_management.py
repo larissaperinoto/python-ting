07192039665dd77f45c994dd676a48e8b70a6af1
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-07 14:20:32 +0200

    feat: insere função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().split('\n')
+
+    except (FileNotFoundError):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
