171423152bfee62e14b06df434d84e16864317d9
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-08 13:35:29 +0200

    reafctor: refatora funções exists_word e search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def exists_word(word, instance):
+def content_with_word(word, instance, callback):
 
     response = []
 
@@ -8,7 +8,7 @@ def exists_word(word, instance):
         for linha, l in enumerate(process['linhas_do_arquivo']):
 
             if word.lower() in l.lower():
-                ocorrencias.append({"linha": linha + 1})
+                ocorrencias.append(callback(linha, l))
 
         if len(ocorrencias) > 0:
             response.append({
@@ -20,23 +20,15 @@ def exists_word(word, instance):
     return response
 
 
+def exists_word(word, instance):
+    def callback(linha, _):
+        return {"linha": linha + 1}
+
+    return content_with_word(word=word, instance=instance, callback=callback)
+
+
 def search_by_word(word, instance):
+    def callback(linha, content):
+        return {"linha": linha + 1, "conteudo": content}
 
-    response = []
-
-    for item, process in enumerate(instance.queue):
-        ocorrencias = []
-
-        for linha, l in enumerate(process['linhas_do_arquivo']):
-
-            if word.lower() in l.lower():
-                ocorrencias.append({"linha": linha + 1, "conteudo": l})
-
-        if len(ocorrencias) > 0:
-            response.append({
-                "palavra": word,
-                "arquivo": instance.queue[item]['nome_do_arquivo'],
-                "ocorrencias": ocorrencias,
-            })
-    print(response)
-    return response
+    return content_with_word(word=word, instance=instance, callback=callback)
