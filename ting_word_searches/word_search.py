264113e02b98d0243214821e5c9367ac4f7be5d5
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-08 13:03:25 +0200

    feat: insere função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    response = []
+
+    for item, process in enumerate(instance.queue):
+        ocorrencias = []
+
+        for linha, l in enumerate(process['linhas_do_arquivo']):
+
+            if word.lower() in l.lower():
+                ocorrencias.append({"linha": linha + 1})
+
+        if len(ocorrencias) > 0:
+            response.append({
+                "palavra": word,
+                "arquivo": instance.queue[item]['nome_do_arquivo'],
+                "ocorrencias": ocorrencias
+            })
+
+    return response
 
 
 def search_by_word(word, instance):
